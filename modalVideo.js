fa3d9c4e486a055c26848f12d7f99df3cd85da9b
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-05-10 10:47:37 -0600

    feat:add modal video for first plan

---
+++ modalVideo.js
@@ -0,0 +1,8 @@
+const video = document.querySelector(".video");
+const modal = document.querySelector(".modal");
+const videoPlanOne = document.querySelector(".plan_one");
+videoPlanOne.addEventListener("mousemove", () => {
+    // modal.classList.add("modal-active");
+    modal.classList.toggle("modal-active");
+   
+});
